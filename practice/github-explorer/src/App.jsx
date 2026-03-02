56451cee2969d897639d4bb3aee64ba1df56c1f7
Rahul Rathore <rrathore959910@gmail.com>
2026-03-02 13:21:45 +0530

    initial commit

---
+++ practice/github-explorer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+  <h1 className="text-5xl font-extrabold text-white">
+    MERN Developer Mode 🚀
+  </h1>
+</div>
+
+  )
+}
+
+export default App
+
